cfd96851d66a7935255f9ec51dcf8d9e0a33c7a1
Kenneth Wong <k.kenneth.w@gmail.com>
2018-11-29 04:42:07 -0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-10-01&endtime=" +
-  "2018-10-07&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+//     "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-10-01&endtime=" +
+//   "2018-10-07&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
